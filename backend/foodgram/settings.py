d885c54c84e3d46e1d31d00b58a75dab931e7957
Kirill Sedyakin <sedyakinki@student.bmstu.ru>
2024-04-23 13:21:17 +0300

    Попытка деплой11

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -103,10 +103,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
