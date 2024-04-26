b44507440231a163d70b97d21fd58adadfba6dc8
Kirill Sedyakin <sedyakinki@student.bmstu.ru>
2024-04-26 08:25:35 +0300

    Админка New

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -55,7 +55,7 @@ class RecipeAdmin(admin.ModelAdmin):
 
     @admin.display(description='Ингредиенты')
     def display_ingredients(self, obj):
-        return ', '.join([ingredient.ingredient.name
+        return ', '.join([ingredient.name
                           for ingredient in obj.recipeingredient.all()])
 
     @admin.display(description='Теги')
