37ae1c8f272727a02fa4fb601b93c1d99f619209
Kirill Sedyakin <sedyakinki@student.bmstu.ru>
2024-05-03 22:59:13 +0300

    2 ревью v2

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,16 +2,10 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path(
-        'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
-    ),
 ]
 
 if settings.DEBUG:
